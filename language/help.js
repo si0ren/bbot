6d477a238d21cd664ea4d3c6a2b5a60174d47553
pnglaj4l <100740491+pnglaj4l@users.noreply.github.com>
2022-04-10 16:52:52 +0700

    Add files via upload

---
+++ language/help.js
@@ -0,0 +1,220 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+// Menu
+exports.menu = () => {
+	return``
+}
+
+// All Menu
+exports.allmenu = (prefix) => {
+return`
+┌── 「 *Owner Menu* 」
+├ ${prefix}react [emoji]
+├ ${prefix}chat [option]
+├ ${prefix}join [link]
+├ ${prefix}leave
+├ ${prefix}block @user
+├ ${prefix}unblock @user
+├ ${prefix}bcgroup [text]
+├ ${prefix}bcall [text]
+├ ${prefix}setppbot [image]
+├ ${prefix}setexif
+└───────
+
+┌── 「 *Info Menu* 」
+├ ${prefix}ping
+├ ${prefix}owner
+├ ${prefix}delete
+├ ${prefix}infochat
+├ ${prefix}quoted
+├ ${prefix}listpc
+├ ${prefix}listgc
+├ ${prefix}listonline
+└───────
+
+┌── 「 *Group Menu* 」
+├ ${prefix}linkgroup
+├ ${prefix}ephemeral [option]
+├ ${prefix}setppgc [image]
+├ ${prefix}setname [text]
+├ ${prefix}setdesc [text]
+├ ${prefix}group [option]
+├ ${prefix}editinfo [option]
+├ ${prefix}add @user
+├ ${prefix}kick @user
+├ ${prefix}hidetag [text]
+├ ${prefix}tagall [text]
+├ ${prefix}antilink [on/off]
+├ ${prefix}mute [on/off]
+├ ${prefix}promote @user
+├ ${prefix}demote @user
+├ ${prefix}vote [text]
+├ ${prefix}devote
+├ ${prefix}upvote
+├ ${prefix}cekvote
+├ ${prefix}hapusvote
+├ ${prefix}jadian
+├ ${prefix}jodohku
+└───────
+
+┌── 「 *Convert Menu* 」
+├ ${prefix}toimage [reply sticker]
+├ ${prefix}removebg [reply image] 
+├ ${prefix}sticker [reply image]
+├ ${prefix}emojimix [em1 + em2]
+├ ${prefix}tovideo [reply sticker]
+├ ${prefix}togif [reply sticker]
+├ ${prefix}tourl [reply gambar/video/gif/dll]
+├ ${prefix}tovn [reply vid/audio]
+├ ${prefix}tomp3 [reply vid/audio]
+├ ${prefix}toaudio [reply vid/audio]
+├ ${prefix}ebinary [reply teks]
+├ ${prefix}dbinary [reply teks]
+├ ${prefix}styletext [teks]
+├ ${prefix}smeme [reply sticker]
+├ ${prefix}halah [teks]
+├ ${prefix}hilih [teks]
+├ ${prefix}huluh [teks]
+├ ${prefix}heleh [teks]
+├ ${prefix}holoh [teks]
+└───────
+
+┌── 「 *Downloader Menu* 」
+├ ${prefix}tiktoknowm [url]
+├ ${prefix}tiktokwm [url]
+├ ${prefix}tiktokmp3 [url]
+├ ${prefix}instagram [url]
+├ ${prefix}twitter [url]
+├ ${prefix}twittermp3 [url]
+├ ${prefix}facebook [url]
+├ ${prefix}pinterestdl [url]
+├ ${prefix}ytmp3 [url]
+├ ${prefix}ytmp4 [url]
+├ ${prefix}getmusic [query]
+├ ${prefix}getvideo [query]
+├ ${prefix}umma [url]
+├ ${prefix}joox [query]
+├ ${prefix}soundcloud [url]
+└───────
+
+┌── 「 *Search Menu* 」
+├ ${prefix}play [query]
+├ ${prefix}yts [query]
+├ ${prefix}google [query]
+├ ${prefix}gimage [query]
+├ ${prefix}pinterest [query]
+├ ${prefix}wallpaper [query]
+├ ${prefix}wikimedia [query]
+├ ${prefix}ytsearch [query]
+├ ${prefix}ringtone [query]
+├ ${prefix}brainly [teks]
+├ ${prefix}lirik [lagu]
+└───────
+
+┌── 「 *Games Menu* 」
+├ ${prefix}family100
+├ ${prefix}tebak lagu
+├ ${prefix}tebak gambar
+├ ${prefix}tebak kata
+├ ${prefix}tebak kalimat
+├ ${prefix}tebak lirik
+├ ${prefix}tebak caklontong
+├ ${prefix}math [mode]
+├ ${prefix}suitpvp [@tag]
+├ ${prefix}delttt
+├ ${prefix}tictactoe
+└───────
+
+┌── 「 *Primbon Menu* 」
+├ ${prefix}nomorhoki
+├ ${prefix}artimimpi
+├ ${prefix}artinama
+├ ${prefix}ramaljodoh
+├ ${prefix}ramaljodohbali
+├ ${prefix}suamiistri
+├ ${prefix}ramalcinta
+├ ${prefix}cocoknama
+├ ${prefix}pasangan
+├ ${prefix}jadiannikah
+├ ${prefix}sifatusaha
+├ ${prefix}rezeki
+├ ${prefix}pekerjaan
+├ ${prefix}nasib
+├ ${prefix}penyakit
+├ ${prefix}tarot
+├ ${prefix}fengshui
+├ ${prefix}haribaik
+├ ${prefix}harisangar
+├ ${prefix}harisial
+├ ${prefix}nagahari
+├ ${prefix}arahrezeki
+├ ${prefix}peruntungan
+├ ${prefix}weton
+├ ${prefix}karakter
+├ ${prefix}keberuntungan
+├ ${prefix}memancing
+├ ${prefix}masasubur
+├ ${prefix}zodiak
+├ ${prefix}shio
+└───────
+
+┌── 「 *Database Menu* 」
+├ ${prefix}setcmd
+├ ${prefix}listcmd
+├ ${prefix}delcmd
+├ ${prefix}lockcmd
+├ ${prefix}addmsg
+├ ${prefix}listmsg
+├ ${prefix}getmsg
+├ ${prefix}delmsg
+└───────
+
+┌── 「 *Anonymous Menu* 」
+├ ${prefix}anonymous
+├ ${prefix}start
+├ ${prefix}next
+├ ${prefix}keluar
+├ ${prefix}sendkontak
+└───────
+
+┌── 「 *Voice Changer* 」
+├ ${prefix}bass
+├ ${prefix}blown
+├ ${prefix}deep
+├ ${prefix}earrape
+├ ${prefix}fast
+├ ${prefix}fat
+├ ${prefix}nightcore
+├ ${prefix}reverse
+├ ${prefix}robot
+├ ${prefix}slow
+├ ${prefix}tupai
+└───────
+
+┌── 「 *Big Thanks To* 」
+├ DikaArdnt
+├ Zeeoneofc
+├ FatihArridho
+├ Sanzy
+├ Yoga
+├ RiychDwayne
+└───────
+`
+}
+
+exports.thanksto = () => {
+	return`
+┌── 「 *Thanks To* 」
+├ DikaArdnt
+├ Zeeoneofc
+├ FatihArridho
+├ Sanzy
+├ Yoga
+├ RiychDwayne
+└───────
+`
+}
